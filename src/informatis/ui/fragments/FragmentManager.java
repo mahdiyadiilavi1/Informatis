40a513cfe4cab0e014aab0d5ad280c3ad8fab5bb
mahdoosh1 <83725163+mahdiyadiilavi1@users.noreply.github.com>
2023-05-14 21:00:39 +0330

    Fixed FragmentManager.java #72
    Fixed #72

--- src/informatis/ui/fragments/FragmentManager.java
+++ src/informatis/ui/fragments/FragmentManager.java
@@ -12,7 +12,7 @@ public class FragmentManager {
         Seq.with(scene.root,
                 ui.picker, ui.editor, ui.controls, ui.restart, ui.join, ui.discord,
                 ui.load, ui.custom, ui.language, ui.database, ui.settings, ui.host,
-                ui.paused, ui.about, ui.bans, ui.admins, ui.traces, ui.maps, ui.content,
+                ui.paused, ui.about, ui.bans, ui.admins, ui.traces, ui.map, ui.content,
                 ui.planet, ui.research, ui.mods, ui.schematics, ui.logic
         ).each(dialog-> dialog.addChild(new ElementViewFragment(dialog)));
 
